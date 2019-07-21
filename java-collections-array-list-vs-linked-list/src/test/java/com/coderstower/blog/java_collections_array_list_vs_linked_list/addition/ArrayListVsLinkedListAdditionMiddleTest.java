6ee078f3fa0006ea68cbc0d25bfca308c4e4a4eb
Daniel Pelaez <estigma88@gmail.com>
2019-07-21 14:01:10 -0500

    Adding jmh to the microbenchmark

--- java-collections-array-list-vs-linked-list/src/test/java/com/coderstower/blog/java_collections_array_list_vs_linked_list/addition/ArrayListVsLinkedListAdditionMiddleTest.java
+++ java-collections-array-list-vs-linked-list/src/test/java/com/coderstower/blog/java_collections_array_list_vs_linked_list/addition/ArrayListVsLinkedListAdditionMiddleTest.java
@@ -1,51 +1,74 @@
 package com.coderstower.blog.java_collections_array_list_vs_linked_list.addition;
 
 
-import org.junit.Ignore;
+import com.coderstower.blog.java_collections_array_list_vs_linked_list.ArrayListExecutionPlan;
+import com.coderstower.blog.java_collections_array_list_vs_linked_list.LinkedListExecutionPlan;
+import com.coderstower.blog.java_collections_array_list_vs_linked_list.Utils;
 import org.junit.Test;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.results.Result;
+import org.openjdk.jmh.results.RunResult;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static com.coderstower.blog.java_collections_array_list_vs_linked_list.Utils.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@Ignore
 public class ArrayListVsLinkedListAdditionMiddleTest {
-
   @Test
-  public void add_middle() {
-    long arrayListTime = 0;
-    long linkedListTime = 0;
-    int valueAdded = amountData / 2;
-
-    for (int i = 1; i <= amountIterations; i++) {
-      Collection<Integer> data = populate(amountData);
-
-      ArrayList<Integer> arrayList = new ArrayList<>(
-              data);
-      LinkedList<Integer> linkedList = new LinkedList<>(
-              data);
-
-      arrayListTime += addToMiddle(arrayList,
-              valueAdded);
-      linkedListTime += addToMiddle(linkedList,
-              valueAdded);
-    }
-
-    printResults("AddToMiddle", arrayListTime,
-            linkedListTime);
-
-    assertThat(calculateAverage(linkedListTime))
-            .isLessThan(
-                    calculateAverage(arrayListTime));
+  public void add_middle()
+          throws RunnerException, IOException {
+    Utils.populateToFile();
+    Options opt = new OptionsBuilder()
+            .include(
+                    ArrayListVsLinkedListAdditionEndTest.class
+                            .getSimpleName())
+            .detectJvmArgs()
+            .build();
+    Collection<RunResult> runResults = new Runner(opt)
+            .run();
+    Result arrayListResult = Utils
+            .find("arrayList",
+                    runResults);
+    Result linkedListResult = Utils
+            .find("linkedList",
+                    runResults);
+    assertThat(linkedListResult.getScore())
+            .isLessThan(arrayListResult.getScore());
   }
 
-  private long addToMiddle(List<Integer> list,
-                           Integer value) {
-    return calculateTime(() -> list
-            .add(value, value));
+  @Benchmark
+  @Measurement(iterations = Utils.amountIterations, batchSize = 1, time = 1)
+  @BenchmarkMode(Mode.SingleShotTime)
+  @Fork(1)
+  @Warmup(iterations = 10)
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  public ArrayList<Integer> arrayList(
+          ArrayListExecutionPlan executionPlan) {
+    ArrayList<Integer> list = executionPlan.arrayList;
+    list.add(list.size() / 2, executionPlan.element);
+    return list;
+  }
+
+  @Benchmark
+  @Measurement(iterations = Utils.amountIterations, batchSize = 1, time = 1)
+  @BenchmarkMode(Mode.SingleShotTime)
+  @Fork(1)
+  @Warmup(iterations = 10)
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  public LinkedList<Integer> linkedList(
+          LinkedListExecutionPlan executionPlan) {
+    LinkedList<Integer> list = executionPlan.linkedList;
+    list.add(list.size() / 2, executionPlan.element);
+    return list;
   }
 }
