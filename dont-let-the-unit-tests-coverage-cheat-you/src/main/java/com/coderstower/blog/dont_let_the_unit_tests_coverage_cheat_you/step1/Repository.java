c627513b2cfd53ee185023afcc24a3d655b29c79
Daniel Pelaez <estigma88@gmail.com>
2019-10-26 16:36:38 -0500

    Adding Tests

---
+++ dont-let-the-unit-tests-coverage-cheat-you/src/main/java/com/coderstower/blog/dont_let_the_unit_tests_coverage_cheat_you/step1/Repository.java
@@ -0,0 +1,5 @@
+package com.coderstower.blog.dont_let_the_unit_tests_coverage_cheat_you.step1;
+
+interface Repository {
+  User save(User user);
+}
---
+++ dont-let-the-unit-tests-coverage-cheat-you/src/main/java/com/coderstower/blog/dont_let_the_unit_tests_coverage_cheat_you/step2/Repository.java
@@ -0,0 +1,6 @@
+package com.coderstower.blog.dont_let_the_unit_tests_coverage_cheat_you.step2;
+
+interface Repository {
+  User save(
+          User user);
+}
