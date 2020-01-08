152a96d740942886aecb4fb5f9aad23d9d9c6471
Daniel Pelaez <estigma88@gmail.com>
2020-01-07 19:41:18 -0500

    Adding templates first step

---
+++ open-close-principle-templates/src/main/java/com/coderstower/blog/open_close_principle_templates/step1/Main.java
@@ -0,0 +1,11 @@
+package com.coderstower.blog.open_close_principle_templates.step1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+  public static void main(String[] args){
+    SpringApplication.run(Main.class, args);
+  }
+}
---
+++ open-close-principle-templates/src/main/java/com/coderstower/blog/open_close_principle_templates/step1/main/BasicTemplateRender.java
@@ -0,0 +1,14 @@
+package com.coderstower.blog.open_close_principle_templates.step1.main;
+
+import com.coderstower.blog.open_close_principle_templates.step1.abstraction.TemplateRender;
+
+import java.util.Map;
+
+public class BasicTemplateRender implements
+        TemplateRender {
+  @Override
+  public String render(String name,
+                       Map<String, Object> data) {
+    return null;
+  }
+}
