aa1f8a56914c4b3cceebeb4b547e012b181bd554
Daniel Pelaez <estigma88@gmail.com>
2020-02-05 12:27:55 -0500

    Adding google account

--- oidc-spring-security/resource-server/src/main/java/com/coderstower/blog/oidc_spring_security/resource/controller/ResourceController.java
+++ oidc-spring-security/resource-server/src/main/java/com/coderstower/blog/oidc_spring_security/resource/controller/ResourceController.java
@@ -1,5 +1,8 @@
 package com.coderstower.blog.oidc_spring_security.resource.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +13,7 @@ import java.util.List;
 public class ResourceController {
 
     @GetMapping("whoarethere")
-    public List<String> whoAreThere() {
-        return Arrays.asList("Daniel", "Pedro", "Juan");
+    public List<String> whoAreThere(@AuthenticationPrincipal Jwt principal) {
+        return Arrays.asList("Daniel", "Pedro", "Juan", "Jwt: " + principal.getClaims());
     }
 }
