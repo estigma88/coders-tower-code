d14b79d4fb58d53a5039bcfb4013eec3531208cf
Daniel Pelaez <estigma88@gmail.com>
2021-11-13 14:12:42 -0500

    Adding new project

---
+++ spring-observability/src/main/java/com/coderstower/blog/springobservability/SpringObservabilityApplication.java
@@ -0,0 +1,13 @@
+package com.coderstower.blog.springobservability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringObservabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringObservabilityApplication.class, args);
+	}
+
+}
---
+++ spring-observability/src/test/java/com/coderstower/blog/springobservability/SpringObservabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderstower.blog.springobservability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringObservabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
