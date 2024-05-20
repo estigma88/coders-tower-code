c7857b9e2a5e5b56516eeba4f1b00df4b3c376e5
Daniel Pelaez <estigma88@gmail.com>
2024-05-20 16:36:37 -0500

    A case for integration tests: an inverted pyramid

--- a-case-for-integration-tests/src/itest/java/com/coderstower/blog/a_case_integration_tests/application/textcontextcache/SecondContextTest.java
+++ a-case-for-integration-tests/src/itest/java/com/coderstower/blog/a_case_integration_tests/application/textcontextcache/SecondContextTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
+@SpringBootTest
 @ActiveProfiles({"itest", "newProfile"})
 public class SecondContextTest {
     @Test
