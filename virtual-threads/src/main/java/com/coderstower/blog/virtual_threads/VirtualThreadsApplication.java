d29d20b4f6a8115742c6ee2644c0af073b55757f
Daniel Pelaez <estigma88@gmail.com>
2025-08-11 18:22:53 -0500

    Virtual threads

---
+++ virtual-threads/src/main/java/com/coderstower/blog/virtual_threads/VirtualThreadsApplication.java
@@ -0,0 +1,13 @@
+package com.coderstower.blog.virtual_threads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualThreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualThreadsApplication.class, args);
+	}
+
+}
